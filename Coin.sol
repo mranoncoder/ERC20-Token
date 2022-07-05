2befc3de30e0c0d180dc9b8ec5c28987513205f4
Abolfazl Arab <64337404+mranoncoder@users.noreply.github.com>
2022-07-05 14:04:08 +0200

    Create Coin.sol

---
+++ Coin.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+
+contract Coin is ERC20 {
+    using SafeMath for uint256;
+    uint256 MaxSupply = 1000000 ether; // max token supply  1 000 000
+
+    constructor() ERC20("My Coin", "MC") {
+        transferOwnership(msg.sender);
+        _mint(msg.sender, MaxSupply); //send 1 000 000 tokens to contract deployer
+    }
+}
